515ccf8e35b5cf6bbee6e1c2e8406c32bab52820
BapanBigData <bapanmldl7892@gmail.com>
2024-07-05 16:57:16 +0530

    changes in BST

--- Tree and BST/BST/BST_advanced.py
+++ Tree and BST/BST/BST_advanced.py
@@ -7,7 +7,19 @@ class TreeNode:
         self.left = left
         self.right = right
 
+## inorder traversal
+def inOrderTraversal(root: TreeNode):
+    ## base case
+    if root is None:
+        return
+    
+    inOrderTraversal(root.left)
+    print(root.val, end=' ')
+    inOrderTraversal(root.right)
+    
+    return
 
+##############################################
 ## to build the tree
 ## insert at BST
 def insert(root: TreeNode, x: int) -> TreeNode:
@@ -25,7 +37,7 @@ def insert(root: TreeNode, x: int) -> TreeNode:
         root.left = insert(root.left, x)
     
     return root
-
+#######################################################################################
 
 ## k-th smallest is BST
 def printKthSmallest(root, k):
@@ -47,7 +59,7 @@ def printKthSmallest(root, k):
     
     return solver(root, k)
 
-
+## **Check the given binary tree is BST or NOT**
 def isValidBST(root) -> bool:
     
     def getMaxVal(root):
@@ -168,7 +180,79 @@ def isValidBST1(root):
         return (root.val > lower) and (root.val < upper) and \
             solveEfficient(root.left, lower, root.val) and solveEfficient(root.right, root.val, upper)
     
-    return solveEfficient(root, -math.inf, math.inf)
+    prev = [-math.inf]
+    
+    def inOrderTraversal(root):
+        if root is None:
+            return True
+        
+        left = inOrderTraversal(root.left)
+        
+        if root.val <= prev[0]:
+            return False
+        
+        prev[0] = root.val
+        
+        right = inOrderTraversal(root.right)
+        
+        return left and right 
+    
+    return inOrderTraversal(root)
+
+
+## **Fix BST with Two Nodes Swapped**
+def recoverTree(root: TreeNode):
+    first = [None]
+    second = [None]
+    prev = [None]
+    
+    def inOrderTraversal(root):
+        if root is None:
+            return
+        
+        inOrderTraversal(root.left)
+        
+        if prev[0] and root.val <= prev[0].val:
+            if first[0] is None:
+                first[0] = prev[0]
+                
+            second[0] = root
+        
+        prev[0] = root
+        
+        inOrderTraversal(root.right)
+        return
+    
+    inOrderTraversal(root)
+    
+    ## swap the first and second
+    first[0].val, second[0].val = second[0].val, first[0].val
+    
+    return 
+
+## **653. Two Sum IV - Input is a BST**
+## Given the root of a binary search tree and an integer k, return true if there exist 
+## two elements in the BST such that their sum is equal to k, or false otherwise.
+
+def findTarget(root: TreeNode, k: int) -> bool:
+    s = set()
+    
+    def solver(root):
+        if root is None:
+            return False
+        
+        left = solver(root.left)
+        
+        if (k-root.val) in s:
+            return True
+        
+        s.add(root.val)
+        
+        right = solver(root.right)
+        
+        return left or right
+    
+    return solver(root)
 
 
 ## let's build the tree
@@ -178,13 +262,25 @@ for val in values:
     root = insert(root, val)
 
 
-root = TreeNode(2)
-root.left = TreeNode(2)
-root.right = TreeNode(2)
+# root = TreeNode(2)
+# root.left = TreeNode(2)
+# root.right = TreeNode(2)
 
+root = TreeNode(20)
+root.left = TreeNode(60)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(10)
+root.right = TreeNode(80)
+root.right.left = TreeNode(8)
+root.right.right = TreeNode(100)
     
 # print(root.left.left.val)
 # printKthSmallest(root, 3)
-print(isValidBST1(root))
+# print(isValidBST1(root))
 
-print(isValidBST(root))
+# print(isValidBST(root))
+inOrderTraversal(root)
+
+recoverTree(root)
+print()
+inOrderTraversal(root)
