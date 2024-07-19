71f17254a8de8d929807d91b0f1b1c5036f94119
BapanBigData <bapanmldl7892@gmail.com>
2024-07-19 17:34:37 +0530

    changes done

--- Tree and BST/Binary Heap/problems_on_heap.py
+++ Tree and BST/Binary Heap/problems_on_heap.py
@@ -35,6 +35,70 @@ def k_largest(nums: list[int], k: int) -> list[int]:
     
     return res
 
-nums = [5, 15, 10, 20, 8, 25, 18]
-k_largest = kLargest(nums, 3)
-print(k_largest)
+# nums = [5, 15, 10, 20, 8, 25, 18]
+# k_largest = kLargest(nums, 3)
+# print(k_largest)
+########################################################################################################################################
+## ** k smallest elements **
+
+def usingMinHeap(nums, k):
+    res = []
+    
+    ## heapify
+    heapq.heapify(nums)
+    
+    for _ in range(k):
+        x = heapq.heappop(nums)
+        res.append(x)
+    
+    return res
+
+def usingMaxHeap(nums, k):
+    nums = [-num for num in nums]
+    
+    pq = nums[:k]
+    heapq.heapify(pq)
+    
+    for e in nums[k:]:
+        if (e > pq[0]):
+            heapq.heapreplace(pq, e)
+    
+    return [-i for i in pq]
+
+## nums = [100, 140, 20, 130, 5, 70, 40, 12]; k = 3
+## print(usingMaxHeap(nums, k))
+
+#######################################################################################################################################
+## ** K closest elements **
+def naiveKClosest(nums, k, x):
+    res = []
+    for _ in range(k):
+        mi = 0
+        for i in range(1, len(nums)):
+            if  abs(nums[i] - x) < abs(nums[mi] - x):
+                mi = i
+        res.append(nums[mi])
+        nums.pop(mi)
+    return res
+
+
+def kClosest(nums: list[int], k: int, x: int):
+    ## Max Heap
+    n = len(nums)
+    pairs = [(-abs(nums[i]-x), i) for i in range(n)]
+    
+    pq = pairs[:k]
+    heapq.heapify(pq)
+    
+    for val, indx in pairs[k:]:
+        if (val > pq[0][0]):
+            heapq.heapreplace(pq, (val, indx))
+    
+    return [nums[indx] for _, indx in pq]
+
+nums = [12, 16, 22, 30, 35, 39, 42, 45, 48, 50, 53, 55, 56]; k = 4; x = 35
+closest = kClosest(nums, k, x)
+print(closest)
+
+
+
