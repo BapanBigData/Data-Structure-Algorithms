61848a9b89b2aa646ae027ab8321868d4014c784
BapanBigData <bapanmldl7892@gmail.com>
2024-08-29 01:39:38 +0530

    updated

--- Arrays/Two Pointers and Sliding Window Approach/1.py
+++ Arrays/Two Pointers and Sliding Window Approach/1.py
@@ -24,8 +24,60 @@ def max_score(nums: list[int], k: int) -> int:
     
     return max_sum
 
+# nums = [1,2,3,4,5,6,1]
+# k = 3
+# res = max_score(nums, k)
+# print(res)
 
-nums = [1,2,3,4,5,6,1]
-k = 3
-res = max_score(nums, k)
+############################################################################################################
+
+# 3. Longest Substring Without Repeating Characters
+
+def length_of_longest_substring(s: str) -> int:
+    
+    def solver():
+        n = len(s)
+        max_len = 0
+
+        for i in range(n):
+            mapp = {}
+            for j in range(i, n):
+                if s[j] in mapp:
+                    break
+                
+                l = j - i + 1
+                max_len = max(max_len, l)
+
+                mapp[s[j]] = 1
+        
+        return max_len 
+    
+    
+    def optimal_solver():
+        n = len(s)
+        
+        max_len = 0
+        
+        l, r = 0, 0
+        mapp = {}
+        
+        while (r < n):
+            
+            if s[r] in mapp:
+                if (mapp[s[r]] >= l):
+                    l = mapp[s[r]] + 1
+            
+            curr_len = r - l + 1
+            max_len = max(max_len, curr_len)
+            
+            mapp[s[r]] = r
+            r += 1
+        
+        return max_len
+    
+    return solver(), optimal_solver()
+
+s = "abba"
+res = length_of_longest_substring(s)
 print(res)
+
